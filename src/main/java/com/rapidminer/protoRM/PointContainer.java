92c00b2a677c67c2ded26a47ef065b49042a3c53
Paweł Wanot <pawel.wanot@gmail.com>
2019-05-29 20:50:20 +0200

    better optimize

--- src/main/java/com/rapidminer/protoRM/PointContainer.java
+++ src/main/java/com/rapidminer/protoRM/PointContainer.java
@@ -8,12 +8,10 @@ import java.util.*;
 
 
 public class PointContainer {
-    private double id_;
     private HashMap<PointType, LinkedList<PointData>> pointListMap_ = new HashMap<>();
-
-    public PointContainer(double id_) {
-        this.id_ = id_;
-    }
+    private int iterator_;
+    private LinkedList<PointData> iterFirst;
+    private LinkedList<PointData> iterSecond;
 
     public void addPoint(PointType type, PointData point) {
         LinkedList<PointData> list = pointListMap_.get(type);
@@ -32,36 +30,42 @@ public class PointContainer {
                 pointListMap_.put(type, list);
             }
         }
+        setFirstPair();
     }
 
     public Tupel<PointData, PointData> getPair() {
-        LinkedList<PointData> myPoints = pointListMap_.get(PointType.MyClass);
-        LinkedList<PointData> otherPoints = pointListMap_.get(PointType.OtherClass);
-        if (myPoints.getFirst().compareTo(otherPoints.getFirst()) <= 0) {
-            return new Tupel<>(myPoints.getFirst(), otherPoints.getFirst());
+        PointData point1 = iterFirst.get(iterator_);
+        PointData point2 = iterSecond.getFirst();
+        if (point1.compareTo(point2) <= 0) {
+            return new Tupel<>(point1, point2);
         } else {
-            return new Tupel<>(otherPoints.getFirst(), myPoints.getFirst());
+            return new Tupel<>(point2, point1);
         }
     }
 
     public void generateNewPair() throws NullPointerException {
+            iterator_++;
+        if (iterSecond.size() == 1 && iterator_ >= iterFirst.size()) {
+            throw new NullPointerException();
+        } else if (iterSecond.size() > 1 && iterator_  == iterFirst.size()) {
+            iterSecond.pollFirst();
+            iterator_ = 0;
+        }
+    }
+
+    private void setFirstPair() {
         LinkedList<PointData> myPoints = pointListMap_.get(PointType.MyClass);
         LinkedList<PointData> otherPoints = pointListMap_.get(PointType.OtherClass);
-        if (myPoints.size() > 1 && otherPoints.size() > 1) {
-            PointData myNextData = myPoints.get(1);
-            PointData otherNextData = myPoints.get(1);
-            if (myNextData.compareTo(otherNextData) >= 0) {
-                myPoints.pollFirst();
-            } else {
-                otherPoints.pollFirst();
-            }
-        } else if (myPoints.size() == 1 && otherPoints.size() > 1) {
-                otherPoints.pollFirst();
-        } else if (myPoints.size() > 1 && otherPoints.size() == 1) {
-                myPoints.pollFirst();
+        PointData my = myPoints.getFirst();
+        PointData other = otherPoints.getFirst();
+        if (my.compareTo(other) <= 0) {
+            iterSecond = myPoints;
+            iterFirst = otherPoints;
         } else {
-            throw new NullPointerException();
+            iterSecond = otherPoints;
+            iterFirst = myPoints;
         }
+        iterator_ = 0;
     }
 
     public long getPairId() {
